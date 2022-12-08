b8472e8bbbcf05c7410d43e3ae5fef377e52066b
Kushagra Saxena <kushagras508@gmail.com>
2022-12-08 19:36:32 +0530

    add a controller and a router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,3 @@
+module.exports.home = function home(req,res){
+    return  ('<h1>Express is up for Allsocial!</h1>')
+}
--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const app = express();
 const port = 8000;
 
 
+app.use('/', require('./routes'));
+
 app.listen(port, function(err){
     if (err){
         console.log(`Error in running the server: ${err}`);
     }
     console.log(`Server is running on port: ${port}`);
-});
+});  
